0281f38d340f460cd2a0b7309b046b9a8be09429
Richard <legalize@xmission.com>
2025-05-14 15:40:59 -0600

    Add std::complex<double> span benchmark

--- libs/include/mandel/span.h
+++ libs/include/mandel/span.h
@@ -6,5 +6,6 @@ namespace mandel
 {
 
 std::vector<int> span_std_complex(float start_re, float start_im, float end_re, float end_im, int num_pixels);
+std::vector<int> span_std_complex(double start_re, double start_im, double end_re, double end_im, int num_pixels);
 
 } // namespace mandel
--- libs/span.cpp
+++ libs/span.cpp
@@ -7,20 +7,29 @@
 namespace mandel
 {
 
-std::vector<int> span_std_complex(float start_re, float start_im, float end_re, float end_im, int num_pixels)
+template <typename T>
+std::vector<int> span(std::complex<T> start, std::complex<T> end, int num_pixels)
 {
-    std::complex<float> c{start_re, start_im};
-    const float step{1.0f / static_cast<float>(num_pixels)};
-    const std::complex<float> delta{(end_re - start_re) * step, (end_im - start_im) * step};
+    std::complex<T> c{start};
+    const std::complex<T> delta{(end - start) / static_cast<T>(num_pixels)};
     std::vector<int> iters;
     iters.reserve(num_pixels);
-
     for (int i = 0; i < num_pixels; ++i)
     {
         iters.push_back(iterate_std_complex(c.real(), c.imag()));
         c += delta;
     }
     return iters;
+}
+
+std::vector<int> span_std_complex(float start_re, float start_im, float end_re, float end_im, int num_pixels)
+{
+    return span(std::complex{start_re, start_im}, std::complex{end_re, end_im}, num_pixels);
+}
+
+std::vector<int> span_std_complex(double start_re, double start_im, double end_re, double end_im, int num_pixels)
+{
+    return span(std::complex{start_re, start_im}, std::complex{end_re, end_im}, num_pixels);
 }
 
 } // namespace mandel
