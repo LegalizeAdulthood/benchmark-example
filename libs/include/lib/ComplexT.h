08e43f40992495b7c48129e410e0772fc49110b4
Richard <legalize@xmission.com>
2025-05-14 11:33:55 -0600

    Add minimal ComplexT class and tests

---
+++ libs/include/lib/ComplexT.h
@@ -0,0 +1,31 @@
+#pragma once
+
+template <typename T>
+struct ComplexT
+{
+    T real{};
+    T imag{};
+
+    ComplexT operator+(T value) const
+    {
+        return {real + value, imag};
+    }
+
+    ComplexT operator+(ComplexT value) const
+    {
+        return {real + value.real, imag + value.imag};
+    }
+
+    ComplexT operator*(T value) const
+    {
+        return {real * value, imag * value};
+    }
+    // (a + bi) * (c + di) = a(c + di) + bi(c + di)
+    //                     = ac + adi + bci + bdi^2
+    //                     = ac + adi + bci - bd
+    //                     = (ac - bd) + (ad + bc)i
+    ComplexT operator*(ComplexT value) const
+    {
+        return {real * value.real - imag * value.imag, real * value.imag + imag * value.real};
+    }
+};
---
+++ tests/ComplexT_test.cpp
@@ -0,0 +1,85 @@
+#include <lib/ComplexT.h>
+
+#include <gtest/gtest.h>
+
+#include <complex>
+
+TEST(TestComplexT, defaultConstruct)
+{
+    ComplexT<float> z;
+
+    EXPECT_EQ(0.0f, z.real);
+    EXPECT_EQ(0.0f, z.imag);
+}
+
+TEST(TestComplexT, constructFromReal)
+{
+    ComplexT<float> z{6.0f};
+
+    EXPECT_EQ(6.0f, z.real);
+    EXPECT_EQ(0.0f, z.imag);
+}
+
+TEST(TestComplexT, constructFromRealImaginary)
+{
+    ComplexT<float> z{6.0f, 3.0f};
+
+    EXPECT_EQ(6.0f, z.real);
+    EXPECT_EQ(3.0f, z.imag);
+}
+
+TEST(TestComplexT, copyConstruct)
+{
+    const ComplexT<float> c{6.0f, 3.0f};
+    ComplexT<float> z{c};
+
+    EXPECT_EQ(6.0f, z.real);
+    EXPECT_EQ(3.0f, z.imag);
+}
+
+TEST(TestComplexT, copyAssign)
+{
+    const ComplexT<float> c{6.0f, 3.0f};
+    ComplexT<float> z = c;
+
+    EXPECT_EQ(6.0f, z.real);
+    EXPECT_EQ(3.0f, z.imag);
+}
+
+TEST(TestComplexT, addReal)
+{
+    const ComplexT<float> c{6.0f, 3.0f};
+    ComplexT<float> z = c + 4.0f;
+
+    EXPECT_EQ(10.0f, z.real);
+    EXPECT_EQ(3.0f, z.imag);
+}
+
+TEST(TestComplexT, addComplex)
+{
+    const ComplexT<float> c{6.0f, 3.0f};
+    ComplexT<float> z = c + c;
+
+    EXPECT_EQ(c.real + c.real, z.real);
+    EXPECT_EQ(c.imag + c.imag, z.imag);
+}
+
+TEST(TestComplexT, multiplyReal)
+{
+    const ComplexT<float> c{6.0f, 3.0f};
+    ComplexT<float> z = c * 2.0f;
+
+    EXPECT_EQ(c.real * 2.0f, z.real);
+    EXPECT_EQ(c.imag * 2.0f, z.imag);
+}
+
+TEST(TestComplexT, multiplyComplex)
+{
+    const ComplexT<float> c{6.0f, 3.0f};
+    ComplexT<float> z = c * c;
+    std::complex<float> expected{std::complex<float>{c.real, c.imag}};
+    expected *= expected;
+
+    EXPECT_EQ(expected.real(), z.real);
+    EXPECT_EQ(expected.imag(), z.imag);
+}
